a01043e144329e6bc864ab7603dbd4e907528d60
Igor P <igor.igorevich08@gmail.com>
2023-06-10 15:05:17 +0300

    add func dif in main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-from funcs import sum_two_nums
+from funcs import sum_two_nums , dif_two_nums
 
 print (sum_two_nums (int (input ()), int (input())))
+print (dif_two_nums (int (input ()), int (input())))
+
